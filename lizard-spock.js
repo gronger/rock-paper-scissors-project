9ef069fcf13ec763df58e0b83fa735335d7f65d8
Joseph Grainger <grrainger7@gmail.com>
2021-06-10 15:02:56 +0200

    finished??????

--- lizard-spock.js
+++ lizard-spock.js
@@ -2,15 +2,33 @@ const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
+
+const cscore = document.getElementById('cscore')
+const pscore = document.getElementById('pscore')
+
+let computerScore = 0;
+let playerScore = 0;
+
 let userChoice;
 let computerChoice;
-possibleChoices.forEach(possibleChoices =>addEventListener('click', (e) =>{
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
     getResult()
 }))
 
+
+function addPlayer() {
+    resultDisplay.innerText = 'you win!!'
+    playerScore = playerScore + 1
+    pscore.innerText = playerScore
+}
+function addComputer() {
+    resultDisplay.innerText = 'you Lost!!'
+    computerScore = computerScore + 1
+    cscore.innerText = computerScore
+}
 
 /* ************************
 
@@ -20,7 +38,7 @@ ROCK, PAPER, SCISSORS, LIZARD, SPOCK
 
 
 function generateComputerChoice() {
-    let randomNumber = Math.floor(Math.random() *  5);
+    let randomNumber = Math.floor(Math.random() *  5) + 1;
 
     if (randomNumber === 1) {
     computerChoice = "rock";
@@ -34,15 +52,15 @@ function generateComputerChoice() {
      if (randomNumber === 4) {
     computerChoice = "lizard";
     } 
-    else {
-    randomNumber = "spock";
+    if (randomNumber === 5) {
+        computerChoice = "spock";
     }
      computerChoiceDisplay.innerHTML = computerChoice;
     }
 
     //=================
 
-    function getResult(){
+    function getResult() {
 
     // computerChoice();
 
@@ -52,64 +70,84 @@ function generateComputerChoice() {
     } 
      if (computerChoice === "rock" && userChoice === "paper") {
         result = 'you win!!  '
+        return addPlayer();
 
     } 
      if (computerChoice === "rock" && userChoice === "scissors") {
-        result =  'you win!!'
+        result =  'you lost !!'
+        return addComputer();
     } 
      if (computerChoice === "rock" && userChoice === "lizard") {
         result = 'you lost!!'
+        return addComputer();
     } 
      if (computerChoice === "rock" && userChoice === "spock") {
         result = 'you win !!'
+        return addPlayer();
     } 
      if (computerChoice === "paper" && userChoice === "rock") {
         result = 'you lost  !!'
+        return addComputer();
     } 
      if (computerChoice === "paper" && userChoice === "scissors") {
         result = 'you win !!'
+        return addPlayer();
     } 
      if (computerChoice === "paper" && userChoice === "lizard") {
         result = 'you win !!'
+        return addPlayer();
     } 
      if (computerChoice === "paper" && userChoice === "spock") {
         result = 'you  lost !!'
+        return addComputer();
     } 
      if (computerChoice === "scissors" && userChoice === "rock") {
         result = 'you win !!'  
+        return addPlayer();
     } 
      if (computerChoice === "scissors" && userChoice === "paper") {
         result = 'you lost !!'
+        return addComputer();
     } 
      if (computerChoice === "scissors" && userChoice === "lizard") {
         result = 'you lost !!'
+        return addComputer();
     } 
      if (computerChoice === "scissors" && userChoice === "spock") {
-        result = 'you  !!'
+        result = 'you win !!'
+        return addPlayer();
     } 
      if (computerChoice === "lizard" && userChoice === "rock") {
         result = 'you win !!'
+        return addPlayer();
     } 
      if (computerChoice === "lizard" && userChoice === "paper") {
         result = 'you  lost !!'
+        return addComputer();
     } 
      if (computerChoice === "lizard" && userChoice === "scissors") {
         result = 'you win !!'
+        return addPlayer();
     } 
      if (computerChoice === "lizard" && userChoice === "spock") {
         result = 'you lost  !!'
+        return addComputer();
     } 
      if (computerChoice === "spock" && userChoice === "rock") {
         result = 'you  lost your wallet !!'
+        return addComputer();
     } 
      if (computerChoice === "spock" && userChoice === "paper") {
         result = 'you win  !!'
+        return addPlayer();
     } 
      if (computerChoice === "spock" && userChoice === "scissors") {
         result = 'you lost  !!'
+        return addComputer();
     } 
      if (computerChoice === "spock" && userChoice === "lizard") {
         result = 'you win !!'
+        return addPlayer();
     }
 resultDisplay.innerHTML = result;
 }
--- single.js
+++ single.js
@@ -4,94 +4,59 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 const cscore = document.getElementById('cscore')
 const pscore = document.getElementById('pscore')
-
-
-
-
-let computerScore = 0 ;
-let playerScore = 0 ;
+let computerScore = 0;
+let playerScore = 0;
 let userChoice;
 let computerChoice;
-let result = "";
-
-
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
-    resultDisplay.innerHTML = result
     generateComputerChoice()
     getResult()
 }))
-
-
 function addPlayer() {
-    playerScore++
+    resultDisplay.innerText = 'you win!!'
+    playerScore = playerScore + 1
     pscore.innerText = playerScore
 }
 function addComputer() {
-    computerScore++
+    resultDisplay.innerText = 'you Lost!!'
+    computerScore = computerScore + 1
     cscore.innerText = computerScore
 }
-
-function generateComputerChoice(){
-    const randomNumber = Math.floor (Math.random() *  3) //possibleChoices.length
-if (randomNumber === 1){
-    computerChoice = 'rock'
- }
- if (randomNumber === 2){
-    computerChoice = 'scissors'
- }
- if (randomNumber === 3){
-    computerChoice = 'paper'
- }
- computerChoiceDisplay.innerHTML = computerChoice
-}    
-
-
-
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 //possibleChoices.length
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
 function getResult() {
-    if (computerChoice === userChoice){
-        result = 'its a draw!'
-        addPlayer(); 
-        addComputer();
+    if (computerChoice === userChoice) {
+        resultDisplay.innerText = 'its a draw!'
     }
-    if (computerChoice === 'rock' && userChoice === "paper"){
-        result = 'you win!!'
+    else if (computerChoice === 'rock' && userChoice === "paper") {
         return addPlayer();
-        // playerScore++ 
-        // pscore.innerText = `player Score: ${playerScore}`
     }
-    if(computerChoice === 'rock' && userChoice ==="scissors"){
-        result = 'you lost !!'
+    else if (computerChoice === 'rock' && userChoice === "scissors") {
         return addComputer();
-    //    computerScore++ 
-    //     cscore.innerText = `computer Score: ${computerScore}`
     }
-    if(computerChoice === 'paper' && userChoice ==="scissors"){
-        result = 'you win!!'
+    else if (computerChoice === 'paper' && userChoice === "scissors") {
         return addPlayer();
-
-        //  playerScore++ 
-        // pscore.innerText = `player Score: ${playerScore}`
     }
-    if(computerChoice === 'paper' && userChoice ==="rock"){
-        result = 'you lost !!'
+    else if (computerChoice === 'paper' && userChoice === "rock") {
         return addComputer();
-
-    //    computerScore++ 
-    //     cscore.innerText = `computer Score: ${computerScore}`
     }
-    if(computerChoice === 'scissors' && userChoice ==="rock"){
-        result = 'you win !!'
+    else if (computerChoice === 'scissors' && userChoice === "rock") {
         return addPlayer();
-
-        //  playerScore++ 
-        // pscore.innerText = `player Score: ${playerScore}`
-     }
-    if(computerChoice === 'scissors' && userChoice ==="paper"){
-        result = 'you lost !!'
+    }
+    else if (computerChoice === 'scissors' && userChoice === "paper") {
         return addComputer();
-
-     }
-
+    }
 }
