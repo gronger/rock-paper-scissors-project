883c3c2d2696d533bac176d42997e8b0028eee8b
Joseph Grainger <grrainger7@gmail.com>
2021-06-10 14:05:40 +0200

    problems SOLVED

--- single.js
+++ single.js
@@ -4,18 +4,37 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 const cscore = document.getElementById('cscore')
 const pscore = document.getElementById('pscore')
+
+
+
+
 let computerScore = 0 ;
 let playerScore = 0 ;
 let userChoice;
 let computerChoice;
-possibleChoices.forEach(possibleChoices =>addEventListener('click', (e) =>{
+let result = "";
+
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    resultDisplay.innerHTML = result
     generateComputerChoice()
     getResult()
 }))
+
+
+function addPlayer() {
+    playerScore++
+    pscore.innerText = playerScore
+}
+function addComputer() {
+    computerScore++
+    cscore.innerText = computerScore
+}
+
 function generateComputerChoice(){
-    const randomNumber = Math.floor (Math.random() *  3) +1  //possibleChoices.length
+    const randomNumber = Math.floor (Math.random() *  3) //possibleChoices.length
 if (randomNumber === 1){
     computerChoice = 'rock'
  }
@@ -27,37 +46,52 @@ if (randomNumber === 1){
  }
  computerChoiceDisplay.innerHTML = computerChoice
 }    
+
+
+
 function getResult() {
     if (computerChoice === userChoice){
         result = 'its a draw!'
+        addPlayer(); 
+        addComputer();
     }
     if (computerChoice === 'rock' && userChoice === "paper"){
         result = 'you win!!'
-        playerScore++ 
-        pscore.innerText = `player Score: ${playerScore}`
+        return addPlayer();
+        // playerScore++ 
+        // pscore.innerText = `player Score: ${playerScore}`
     }
     if(computerChoice === 'rock' && userChoice ==="scissors"){
         result = 'you lost !!'
-       computerScore++ 
-        cscore.innerText = `computer Score: ${computerScore}`
+        return addComputer();
+    //    computerScore++ 
+    //     cscore.innerText = `computer Score: ${computerScore}`
     }
     if(computerChoice === 'paper' && userChoice ==="scissors"){
         result = 'you win!!'
-         playerScore++ 
-        pscore.innerText = `player Score: ${playerScore}`
+        return addPlayer();
+
+        //  playerScore++ 
+        // pscore.innerText = `player Score: ${playerScore}`
     }
     if(computerChoice === 'paper' && userChoice ==="rock"){
         result = 'you lost !!'
-       computerScore++ 
-        cscore.innerText = `computer Score: ${computerScore}`
+        return addComputer();
+
+    //    computerScore++ 
+    //     cscore.innerText = `computer Score: ${computerScore}`
     }
     if(computerChoice === 'scissors' && userChoice ==="rock"){
         result = 'you win !!'
-         playerScore++ 
-        pscore.innerText = `player Score: ${playerScore}`
+        return addPlayer();
+
+        //  playerScore++ 
+        // pscore.innerText = `player Score: ${playerScore}`
      }
     if(computerChoice === 'scissors' && userChoice ==="paper"){
         result = 'you lost !!'
+        return addComputer();
+
      }
-     resultDisplay.innerHTML = result
+
 }
